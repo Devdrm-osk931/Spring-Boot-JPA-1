86568e6e3e655b135d3a2dbc499117774e5891e3
Seok Kyo Oh <osk931@naver.com>
2022-08-09 00:57:52 +0900

    Spring Boot & JPA

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="dtype")
-@Getter @Setter
+@Getter
 public class Item {
 
     @Id
@@ -27,6 +28,31 @@ public class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //==비즈니스 로직==//
+    // 해당 비즈니스 로직을 통해 StockQuantity를 수정한다(Setter 사용 x)
+    // Item Entity가 stockQuantity 값을 갖고 있기 때문에, 해당 값과 관련된 비즈니스 로직을 Item에 구현
+
+    /**
+     * Stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * Stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+    }
 
 
 }
